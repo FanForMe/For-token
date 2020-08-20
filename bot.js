26ab340fd223c392f590201b02bb70b2330e5fae
FanForMe <66275701+FanForMe@users.noreply.github.com>
2020-08-20 22:52:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['295997309013983233'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
